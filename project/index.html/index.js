648b0e8d7de99add81f7ad317783f2685124bcb7
Sam <DaBaccax@gmail.com>
2020-09-11 18:13:10 -0700

    User asked to input Decimal Number; Calulator attemp

---
+++ project/index.html/index.js
@@ -0,0 +1,8 @@
+{
+    var input = prompt("Insert a decimal number");
+
+    console.log(input + " in decimal to decimal: ", input);
+    console.log(input + " in decimal to binary: ", input.toString(2));
+    console.log(input + " in decimal to hex: ", input.toString(16));
+
+}
