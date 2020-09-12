a1eda1f747af002e3720ca49517f19d39b930f72
Alisson-Corrales <alisson-corrales24@gmail.com>
2020-09-11 18:25:49 -0700

    started hexadecimal equation

--- project/index.html/index.js
+++ project/index.html/index.js
@@ -1,8 +1,25 @@
-{
-    var input = prompt("Insert a decimal number");
+let input = prompt("Insert a decimal number");
 
-    console.log(input + " in decimal to decimal: ", input);
-    console.log(input + " in decimal to binary: ", input.toString(2));
-    console.log(input + " in decimal to hex: ", input.toString(16));
+let remainder1 = prompt/16
 
-}
+    if (remainder1 == 10) {
+       remainder1 = "A";
+    }
+    if (remainder1 == 11) {
+        remainder1 = "B";
+     }
+     if (remainder1 == 12) {
+        remainder1 = "C";
+     }
+     if (remainder1 == 13) {
+        remainder1 = "D";
+     }
+     if (remainder1 == 14) {
+        remainder1 = "E";
+     }
+     if (remainder1 == 15) {
+        remainder1 = "F";
+     }
+let hexadecimal = remainder1
+
+console.log(`hexadecimal: ${hexadecimal}`);
