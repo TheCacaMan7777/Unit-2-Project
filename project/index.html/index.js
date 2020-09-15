b1ce82dfb539cb7f1e6cf767eff152bcd9e33366
Sam <DaBaccax@gmail.com>
2020-09-14 17:03:56 -0700

    Binary Code

--- project/index.html/index.js
+++ project/index.html/index.js
@@ -1,25 +1,7 @@
-let input = prompt("Insert a decimal number");
-
-let remainder1 = prompt/16
-
-    if (remainder1 == 10) {
-       remainder1 = "A";
-    }
-    if (remainder1 == 11) {
-        remainder1 = "B";
-     }
-     if (remainder1 == 12) {
-        remainder1 = "C";
-     }
-     if (remainder1 == 13) {
-        remainder1 = "D";
-     }
-     if (remainder1 == 14) {
-        remainder1 = "E";
-     }
-     if (remainder1 == 15) {
-        remainder1 = "F";
-     }
-let hexadecimal = remainder1
-
-console.log(`hexadecimal: ${hexadecimal}`);
+{
+    var input = Number(prompt("Put in a number"));
+ 
+    console.log(input + "in decimal to decimal: " + input)
+    console.log(input + "in decimal to binary: " + input.toString(2));
+    console.log(input + "in decimal to hex: " + input.toString(16));       
+}
